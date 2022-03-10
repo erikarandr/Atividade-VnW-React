2110bfadd8d71a3c38dc0d5f7d24a30750a58c8c
Erika Andrade <andrade_ericka@hotmail.com>
2022-03-10 18:55:43 -0300

    calculadora e to do react

--- atividades-react/src/App.js
+++ atividades-react/src/App.js
@@ -1,170 +1,157 @@
-// Crie uma class component 
 
 
-// Guarde em um state as seguintes propriedades: nome, idade, comida favorita, e uma array com pelo menos 3 músicas.
-
-// Renderize na tela:
-// Seu nome em um h1
-// Sua idade em um h2
-// Sua comida favorita em um h3
-// Suas músicas favoritas em uma lista (ul / ol)
-
-
-
+//Entre no link abaixo e continue a desenvolver a nossa calculadora
+//Você deve criar as funções de cada button para disparar a operação no click
+//Sua calculadora deve ter um button para limpar o resultado da tela e limpar os inputs
+//Você deve buscar a solução para a operação de soma ("+" deve somar e não concatenar)
 
 import React, { Component } from "react";
 
-export default class App extends Component {
-  state={
-    pessoa: [
-      {
-        nome:"Erika",
-        idade:29,
-        comidaFavorita:"lasanha",
-        musica: ["rap","funk","pop"]
-      }
-    ]
+import "./styles.css";
+
+export class App extends Component {
+  state = {
+    n1: "",
+    n2: "",
+    res: ""
+  };
+
+  adi = () => {
+    this.setState({
+      res: Number(this.state.n1) + Number(this.state.n2)
+    });
+  };
+
+  sub = () => {
+    this.setState({
+      res: Number(this.state.n1) - Number(this.state.n2)
+    });
+  };
+
+  mul = () => {
+    this.setState({
+      res: Number(this.state.n1) * Number(this.state.n2)
+    });
+  };
+
+  div = () => {
+    this.setState({
+      res: Number(this.state.n1) / Number(this.state.n2)
+    });
+  };
+
+  handleChange2 = (event) => {
+    this.setState({
+      n2: event.target.value
+    });
+  };
+
+  handleChange1 = (event) => {
+    this.setState({
+      n1: event.target.value
+    });
+  };
+
+  clean = () => {
+    this.setState({
+      n1: "",
+      n2: "",
+      res: ""
+    });
+  };
+
+  render() {
+    return (
+      <div className="bodyContent">
+        <section className="calc">
+          <h1>Calc App</h1>
+
+          <section className="boxInputsNumber">
+            <input onChange={this.handleChange1} value={this.state.n1} />
+            <input onChange={this.handleChange2} value={this.state.n2} />
+          </section>
+          <h2>{this.state.res}</h2>
+
+          <section className="boxButtonsNumbers">
+            <button onClick={this.adi}>+</button>
+
+            <button onClick={this.sub}>-</button>
+
+            <button onClick={this.mul}>*</button>
+
+            <button onClick={this.div}>/</button>
+
+            <button onClick={this.clean}>C</button>
+          </section>
+        </section>
+      </div>
+    );
   }
-  
+}
+
+export default App;
+
+
+//Complete as funções do ToDo
+//Ao adicionar uma tarefa o seu input deve ser limpo
+//Seu ToDo deve ter uma função para adicionar tarefa
+//Seu ToDo deve ter uma função para remover tarefa
+//Seu ToDo deve adicionar tarefas com o click no button e também com a tecla ENTER
+
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    task: "",
+    taskList: []
+  };
+
+  add = () => {
+    const { taskList, task } = this.state;
+
+    if (task === "") return;
+
+    const newTask = {
+      id: new Date(),
+      text: task
+    };
+
+    this.setState({
+      taskList: taskList.concat(newTask)
+    });
+    console.log(this.state.taskList);
+  };
+
+  remove = (id) => {
+    const { taskList } = this.state;
+
+    this.setState({
+      taskList: taskList.filter((item) => item.id !== id)
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      task: event.target.value
+    });
+  };
+
   render() {
     return (
       <div>
-        <h1>O nome é: {this.state.pessoa[0].nome}</h1>
-        <h2>Idade é: {this.state.pessoa[0].idade}</h2>
-        <h3>Comida favortira é: {this.state.pessoa[0].comidaFavorita}</h3>
+        <input onChange={this.handleChange} value={this.state.task} />
+        <button onClick={this.add}>add</button>
         <ul>
-          <li>Estilo musical: {this.state.pessoa[0].musica[o]}</li>
-          <li>Estilo musical: {this.state.pessoa[0].musica[1]}</li>
-          <li>Estilo musical: {this.state.pessoa[0].musica[2]}</li>
+          {this.state.taskList.map((item) => (
+            <li key={item.id}>
+              {item.text}
+              <button onClick={() => this.remove(item.id)}> Remover </button>
+            </li>
+          ))}
         </ul>
       </div>
     );
   }
 }
 
-//Crie um component de class
-
-
-// Crie uma arrow function que receba uma mensagem e exiba na tela
-
-
-// Crie uma arrow function que retorne o dobro de um número e exiba na tela
-
-import React, { Component } from "react";
-
-export default class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello </h1>
-      </div>
-    );
-  }
-}
-
-import React, { Component } from "react";
-
-export default class App extends Component {
-  soma = () => {
-    return 2 + 2;
-  };
-  render() {
-    return (
-      <div>
-        <h1> {this.soma()} </h1>
-      </div>
-    );
-  }
-}
-
-//Crie um component de class
-
-
-//Crie um array de objetos no STATE com pelo menos 6 objetos
-
-
-//Faça um map do seu state e exiba os dados na tela
-
-import React, { Component } from "react";
-
-export default class App extends Component {
-state ={
-  dados: [{
-    nome: "Erika",
-    telefone: 99999-9999,
-  },
-  {
-    nome: "Ana",
-    telefone: 99999-9999,
-
-  }
-  {
-    nome: "Paulo",
-    telefone: 99999-9999,
-  }
-  {
-    nome: "Fred",
-    telefone: 99999-9999,
-  }
-  {
-    nome: "Nat",
-    telefone: 99999-9999,
-  }
-  {
-    nome: "Pedro",
-    telefone: 99999-9999,
-  }
-
-  ]
-}
-
-render (){
-  return(
-    <div>
-      {this.state.dados.map((item) => (
-        <div>
-          <p>Nome:{item.nome}</p>
-          <p>Tel:{item.telefone} </p>
-          </div>
-      ))}
-    </div>
-  )
-}
-
-//Desenvolva um contador que tenha as seguintes funcionalidades:
-//1 - O contador não pode chegar abaixo de 0
-//2 - O contador não pode chegar acima de 10
-
-import React, { Component } from "react";
-
-export default class App extends Component {
-state ={
-  cont: 0
-}
-
-adicionar = () => {
-  if (this.state.cont <10){
-    this.setState({
-      cont:this.state.cont +1
-    })
-  }
-}
-
-diminuir = () =>{
-  if (this.state.cont <10){
-    this.setState({
-      cont:this.state.cont -1
-    })
-  }
-}
-
-render(){
-  return(
-    <div className='container'>
-      <h1 className='titulo'>Contador</h1>
-      <h2 className='cont'>{this.state.cont}</h2>
-      <button className="first-button" onClick={this.adicionar}>+</button>
-      <button className="second-button" onClick={this.retirar}>-</button>
-    </div>
-  )
-}
+export default App;
